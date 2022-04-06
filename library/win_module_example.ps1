8b7c9d6c4777ca9def1e8b03f82ed16b7f2c26a0
DevManTillis <migueltillisjr@gmail.com>
2022-04-06 17:03:30 -0400

    complete basic win module

--- library/win_module_example.ps1
+++ library/win_module_example.ps1
@@ -1,20 +1,27 @@
 #!powershell
+#
+#Requires -Module Ansible.ModuleUtils.Legacy
+#Requires -Module Ansible.ModuleUtils.Backup
 
-# Include the module util ServiceAuth.psm1 from the my_namespace.my_collection collection
-#AnsibleRequires -PowerShell ansible_collections.my_namespace.my_collection.plugins.module_utils.ServiceAuth
+$ErrorActionPreference = 'Stop'
 
-# Create the module spec like normal
-$spec = @{
-    options = @{
-        resource_id = @{ type = 'str'; required = $true }
-        state = @{ type = 'str'; choices = 'absent', 'present' }
-    }
+$params = Parse-Args -arguments $args -supports_check_mode $true
+
+$result = @{
+    changed = $false
 }
 
-# Create the module from the module spec but also include the util spec to merge into our own.
-$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec, @(Get-MyNamespaceMyCollectionServiceAuthSpec))
+Function Do-Something() {
+}
 
-# Call the ServiceAuth module util and pass in the module object so it can access the module options.
-Invoke-MyServiceResource -Module $module -ResourceId $module.Params.resource_id -State $module.params.state
 
-$module.ExitJson()
+#$src = Get-AnsibleParam -obj $params -name "src" -type "path" -failifempty ($copy_mode -in @("explode","process","single"))
+#$dest = Get-AnsibleParam -obj $params -name "dest" -type "path" -failifempty $true
+#$backup = Get-AnsibleParam -obj $params -name "backup" -type "bool" -default $false
+#throw "invalid base64 length '$archive_name'"
+#$parent_dir = [System.IO.Path]::GetDirectoryName($dirpath)
+#Add-Type -AssemblyName System.IO.Compression | Out-Null
+#$result.changed = $true
+#Fail-Json -obj $result -message "Destination directory '$parent_dir' does not exist"
+
+Exit-Json -obj $result
---
+++ library/win_module_example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['stableinterface'],
+                    'supported_by': 'core'}
+
+DOCUMENTATION = r'''
+---
+'''
+
+EXAMPLES = r'''
+'''
+
+RETURN = r'''
+'''
